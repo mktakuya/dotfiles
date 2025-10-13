6f930f357feafd17bec79080482260177b2bf8fc
Takuya Mukohira <takuya.mk96@gmail.com>
2025-10-13 17:41:12 +0900

    ~/.claude/CLAUDE.md だけをGit管理するようにする

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ cd $(dirname $0)
 mkdir -p ~/.vim
 mkdir -p ~/.config
 mkdir -p ~/.peco
+mkdir -p ~/.claude
 
 for dotfile in .?*
 do
@@ -13,7 +14,9 @@ do
   fi
 done
 
+ln -Fis "$PWD/CLAUDE.global.md" "$HOME/.claude/CLAUDE.md"
+
 # Link dot_* directories to home
 ln -Fis "$PWD/dot_config/nvim" "$HOME/.config/"
-ln -Fis "$PWD/dot_claude" "$HOME/.claude"
 ln -Fis "$PWD/dot_peco" "$HOME/.peco"
+

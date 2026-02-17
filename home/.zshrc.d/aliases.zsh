bc5d3daaaa847f90bfe9d8ae25473cc6616c932f
Takuya Mukohira <takuya.mk96@gmail.com>
2026-02-17 22:31:11 +0900

    シェルの起動時、 gcloud コマンドが存在しなくてもエラーにならないようにする

--- home/.zshrc.d/aliases.zsh
+++ home/.zshrc.d/aliases.zsh
@@ -6,7 +6,7 @@ alias ls="ls --color"
 alias sl="ls"
 alias be="bundle exec"
 
-if [ -x "`which gcloud`" ]; then
+if type "gcloud" > /dev/null 2>&1; then
   alias gae-ssh='(){ gcloud --project $1 app instances ssh $(gcloud --project $1 --format json app instances list --service $2 --sort-by="~instance.startTime" | jq -r ".[0].id") --service $2 --version $(gcloud --project $1 --format json app instances list --service $2 --sort-by="~instance.startTime" | jq -r ".[0].version") }'
   alias gae-scp='function _gae_scp(){ pj="$1"; sv="$2"; inst=$(gcloud app instances list --project "$pj" --service "$sv" --format=json --sort-by="~instance.startTime" | jq -r ".[0].id"); ver=$(gcloud app instances list --project "$pj" --service "$sv" --format=json --sort-by="~instance.startTime" | jq -r ".[0].version"); gcloud app instances scp --project "$pj" --service "$sv" --version "$ver" "$inst"; }; _gae_scp'
   alias gae-scp-pull='function _g(){ pj="$1"; sv="$2"; rpath="$3"; lpath="$4"; inst=$(gcloud app instances list --project "$pj" --service "$sv" --format=json --sort-by="~instance.startTime" | jq -r ".[0].id"); ver=$(gcloud app instances list --project "$pj" --service "$sv" --format=json --sort-by="~instance.startTime" | jq -r ".[0].version"); gcloud app instances scp --project "$pj" --service "$sv" --version "$ver" "$inst:$rpath" "$lpath"; }; _g'

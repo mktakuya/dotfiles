ccf9d1755bdd96f68b097da2282e147459b1eb4c
Takuya Mukohira <takuya.mk96@gmail.com>
2026-08-16 11:39:44 +0900

    iTerm2 Shell Integration をリポジトリで管理する
    新規マシンのセットアップ時に iTerm2 のメニュー操作が不要になるよう、
    公式のシェル統合スクリプトをリポジトリに含める。ローダーは他の
    ツール設定と同様に .zshrc.d/ に置く。スクリプトは GPLv2+ なので
    ヘッダーのライセンス表示を残したまま配布する。

---
+++ home/.zshrc.d/iterm2.zsh
@@ -0,0 +1,7 @@
+# 取得元: https://iterm2.com/shell_integration/zsh
+# ライセンス: GPLv2+（ファイルヘッダーの表明を参照のこと）
+# 更新するときは上記 URL から取得してコミットする（iTerm2 のメニューから
+# Install Shell Integration を実行すると symlink が実ファイルで上書きされる）
+if [[ -f $HOME/.iterm2_shell_integration.zsh ]]; then
+  source $HOME/.iterm2_shell_integration.zsh
+fi
